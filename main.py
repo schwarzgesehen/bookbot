59dc2601e4009d2f48cbf37abbf47c080df57880
schwarzgesehen <max.mustermann@test.de>
2024-10-01 00:11:01 +0200

    Added count-words function

--- main.py
+++ main.py
@@ -4,11 +4,16 @@ def get_book_content(path_to_file):
     return book_contents
 
 def count_words(content):
+    count = 0
     words = content.split()
- 
+    for word in words:
+        count += 1
+    return count
 
 def main():
     content = get_book_content("books/frankenstein.txt")
+    count = count_words(content)
     print(content)
+    print(count)
 
 main()
